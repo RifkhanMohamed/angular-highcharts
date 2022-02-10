6daefb2d538f66cb9cd7d3f552261dcb86f81c1a
unknown <rifkhan.mohamed@zenerconsulting.com>
2022-02-10 17:28:25 +0530

    chart options added

---
+++ src/app/_helpers/pieChartOption.ts
@@ -0,0 +1,48 @@
+import { Options } from "highcharts";
+
+export const pieChartOption: Options={
+    chart: {
+        plotBackgroundColor: '',
+        plotBorderWidth: 0,
+        plotShadow: false,
+        type: 'pie'
+      },
+      title: {
+        text: 'Browser market shares in January, 2018'
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      accessibility: {
+        point: {
+          valueSuffix: '%'
+        }
+      },
+      credits:{
+        enabled:false
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+            connectorColor: 'silver'
+          }
+        }
+      },
+      series: [
+        {
+        type: 'pie',
+        data: [
+          { name: 'Chrome', y: 61.41 },
+          { name: 'Internet Explorer', y: 11.84 },
+          { name: 'Firefox', y: 10.85 },
+          { name: 'Edge', y: 4.67 },
+          { name: 'Safari', y: 4.18 },
+          { name: 'Other', y: 7.05 }
+        ]
+      }]
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Chart } from 'angular-highcharts';
+import { pieChartOption } from "./_helpers/pieChartOption";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-charts';
+  pieChart = new Chart(pieChartOption);
 }
